5ac6e4035def5b2e14e7a8eea8458a9bd39892ff
Legacy <yugi.t@qq.com>
2020-11-05 13:15:01 +0800

    Good

--- game_functions.py
+++ game_functions.py
@@ -6,7 +6,7 @@ from alien import Alien
 from time import sleep
 
 
-def check_keydown_events(event, ai_settings, screen, ship, bullets):
+def check_keydown_events(event, ai_settings, screen, ship, bullets, stats, aliens):
     if event.key == pygame.K_RIGHT:
         ship.moving_right = True
     elif event.key == pygame.K_LEFT:
@@ -15,8 +15,8 @@ def check_keydown_events(event, ai_settings, screen, ship, bullets):
         fire_bullet(ai_settings, screen, ship, bullets)
     elif event.key == pygame.K_q:
         sys.exit()
-    # elif event.key==pygame.K_p:
-    #     start_game()
+    elif event.key == pygame.K_p:
+        start_game(ai_settings, screen, stats, ship, aliens, bullets)
 
 
 def fire_bullet(ai_settings, screen, ship, bullets):
@@ -41,7 +41,8 @@ def check_events(ai_settings, screen, stats, play_button, ship, aliens, bullets)
             sys.exit()
 
         elif event.type == pygame.KEYDOWN:
-            check_keydown_events(event, ai_settings, screen, ship, bullets)
+            check_keydown_events(event, ai_settings, screen, ship, bullets, stats, aliens
+                                 )
 
         elif event.type == pygame.KEYUP:
             check_keyup_events(event, ship)
@@ -55,12 +56,13 @@ def check_play_button(ai_settings, screen, stats, play_button, ship, aliens, bul
     """在玩家单击Play按钮时开始新游戏"""
     button_clicked = play_button.rect.collidepoint(mouse_x, mouse_y)
     if button_clicked and not stats.game_active:
+        # 重置游戏设置
+        ai_settings.initialize_dynamic_settings()
         # 隐藏光标
         pygame.mouse.set_visible(False)
         # 重置游戏统计信息
         stats.reset_stats()
         stats.game_active = True
-
         # 清空外星人列表与子弹列表
         aliens.empty()
         bullets.empty()
@@ -69,7 +71,21 @@ def check_play_button(ai_settings, screen, stats, play_button, ship, aliens, bul
         create_fleet(ai_settings, screen, ship, aliens)
 
 
-# def start_game():
+def start_game(ai_settings, screen, stats, ship, aliens, bullets):
+    if not stats.game_active:
+        # 重置游戏设置
+        ai_settings.initialize_dynamic_settings()
+        # 隐藏光标
+        pygame.mouse.set_visible(False)
+        # 重置游戏统计信息
+        stats.reset_stats()
+        stats.game_active = True
+        # 清空外星人列表与子弹列表
+        aliens.empty()
+        bullets.empty()
+
+        # 创建一群新的外星人,让飞船居中
+        create_fleet(ai_settings, screen, ship, aliens)
 
 
 def update_screen(ai_settings, screen, stats, ship, aliens, bullets, play_button):
@@ -104,8 +120,9 @@ def check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets):
     # 消除相应子弹和外星人
     collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
     if len(aliens) == 0:
-        # 删除现有子弹并新建一群外星人
+        # 删除现有子弹并新建一群外星人,加快游戏节奏
         bullets.empty()
+        ai_settings.increase_speed()
         create_fleet(ai_settings, screen, ship, aliens)
 
 
--- settings.py
+++ settings.py
@@ -2,14 +2,14 @@ class Settings():
     """存储游戏中所有设置的类"""
 
     def __init__(self):
-        """初始化游戏的设置"""
+        """初始化游戏的静态设置"""
         # 屏幕设置
         self.screen_width = 1300
         self.screen_height = 700
         self.bg_color = (100, 230, 230)
 
-        #飞船设置
-        self.ship_limit=2
+        # 飞船设置
+        self.ship_limit = 2
         self.ship_speed_factor = 3
 
         # 子弹设置
@@ -18,12 +18,29 @@ class Settings():
         self.bullet_height = 15
         self.bullet_color = 60, 60, 60
         self.bullets_allowed = 3
-        
+
         # 外星人设置
         self.alien_speed_factor = 1
         self.fleet_drop_speed = 10
-        #fleet_direction:1向右,-1向左
+        # fleet_direction:1向右,-1向左
         self.fleet_direction = 1
-        
-        
-        
+
+        # 以什么样的速度加快游戏节奏
+        self.speedup_scale = 1
+
+        self.initialize_dynamic_settings()
+
+    def initialize_dynamic_settings(self):
+        """初始化随游戏进行而变化的设置"""
+        self.ship_speed_factor = 1.5
+        self.bullet_speed_factor = 3
+        self.alien_speed_factor = 1
+
+        # fleet_direction为1表示向右，-1表示向左
+        self.fleet_direction = 1.1
+
+    def increase_speed(self):
+        """提高速度设置"""
+        self.ship_speed_factor *= self.speedup_scale
+        self.bullet_speed_factor *= self.speedup_scale
+        self.alien_speed_factor *= self.speedup_scale
